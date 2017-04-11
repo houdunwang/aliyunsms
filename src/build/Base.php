1d4df4fa21fb99cc1dda4f1b13662783228a5258
houdunwang <2300071698@qq.com>
2017-04-11 12:07:06 +0800

    no message

--- src/build/Base.php
+++ src/build/Base.php
@@ -7,8 +7,9 @@
  * |    WeChat: aihoudun
  * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
  * '-------------------------------------------------------------------*/
+
 namespace houdunwang\aliyunsms\build;
-include_once __DIR__.DIRECTORY_SEPARATOR.'aliyun-php-sdk-core/Config.php';
+include_once __DIR__ . DIRECTORY_SEPARATOR . 'aliyun-php-sdk-core/Config.php';
 use houdunwang\config\Config;
 use Sms\Request\V20160927 as Sms;
 
@@ -18,12 +19,13 @@ use Sms\Request\V20160927 as Sms;
  * @package houdunwang\aliyunsms\build
  */
 class Base {
+	protected $error;
+
 	/**
 	 * 发送短信
 	 *
 	 * @param array $data
 	 * $data=[
-	 *  'sign'=>'签名名称',
 	 *  'code'=>'模板code',
 	 *  'mobile'=>'手机号',
 	 *  //以下是模板中的变量
@@ -31,35 +33,51 @@ class Base {
 	 *      'code'=>333,
 	 *  ]
 	 * ]
+	 *
+	 * @return bool
 	 */
 	public function send( array $data ) {
-		chdir(__DIR__);
+		chdir( __DIR__ );
 		$iClientProfile = \DefaultProfile::getProfile( "cn-hangzhou",
-			Config::get( 'aliyunsms.accessKey' ), Config::get( 'aliyunsms.accessSecret' ) );
+			Config::get( 'aliyunsms.accessKey' ),
+			Config::get( 'aliyunsms.accessSecret' )
+		);
 		$client         = new \DefaultAcsClient( $iClientProfile );
 		$request        = new Sms\SingleSendSmsRequest();
 
 		/*签名名称*/
-		$request->setSignName( $data['sign'] );
+		$request->setSignName( Config::get( 'aliyunsms.sign' ) );
 		/*模板code*/
-		$request->setTemplateCode( $data['code'] );
+		$request->setTemplateCode( $data['template_code'] );
 		/*目标手机号*/
 		$request->setRecNum( $data['mobile'] );
 		$request->setParamString( json_encode( $data['vars'], JSON_UNESCAPED_UNICODE ) );
-//		$vars = [
-//			'code'    => '9999',
-//			'product' => '后盾人'
-//		];
-//		$request->setParamString( json_encode( $vars, JSON_UNESCAPED_UNICODE ) );
+
+		chdir( dirname( $_SERVER['SCRIPT_FILENAME'] ) );
 		try {
-			$response = $client->getAcsResponse( $request );
-			print_r( $response );
+			$client->getAcsResponse( $request );
+
+			return true;
 		} catch ( \ClientException  $e ) {
-			print_r( $e->getErrorCode() );
-			print_r( $e->getErrorMessage() );
+//			print_r( $e->getErrorCode() );
+//			print_r( $e->getErrorMessage() );
+			$this->error = $e->getErrorMessage();
+
+			return false;
 		} catch ( \ServerException  $e ) {
-			print_r( $e->getErrorCode() );
-			print_r( $e->getErrorMessage() );
+//			print_r( $e->getErrorCode() );
+//			print_r( $e->getErrorMessage() );
+			$this->error = $e->getErrorMessage();
+
+			return false;
 		}
+	}
+
+	/**
+	 * 获取错误信息
+	 * @return mixed
+	 */
+	public function getError() {
+		return $this->error;
 	}
 }
