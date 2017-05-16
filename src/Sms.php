80d753ea9fbb85d3a7428eed66774c89fb859478
houdunwang <2300071698@qq.com>
2017-05-16 22:43:30 +0800

    no message

--- src/Sms.php
+++ src/Sms.php
@@ -10,43 +10,22 @@
 
 namespace houdunwang\aliyunsms;
 
-use houdunwang\aliyunsms\build\Base;
-
-
 class Sms
 {
     protected $link;
 
-    //更改缓存驱动
-    protected function driver()
-    {
-        $this->link = new Base();
-
-        return $this;
-    }
-
     public function __call($method, $params)
     {
         if (is_null($this->link)) {
-            $this->driver();
+            $this->link = new Base();
         }
         if (method_exists($this->link, $method)) {
             return call_user_func_array([$this->link, $method], $params);
         }
     }
 
-    public static function single()
-    {
-        static $link;
-        if (is_null($link)) {
-            $link = new static();
-        }
-
-        return $link;
-    }
-
     public static function __callStatic($name, $arguments)
     {
-        return call_user_func_array([static::single(), $name], $arguments);
+        return call_user_func_array([new static(), $name], $arguments);
     }
 }
